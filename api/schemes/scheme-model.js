2b6dae7482ca1ebf7a2847b5dfc48384ddbdbb57
jMisch44 <jennimisch@gmail.com>
2021-10-06 18:07:05 -0400

    findSteps model completed

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -38,7 +38,12 @@ async function findById(scheme_id) {
   return result;
 }
 
-function findSteps(scheme_id) { // EXERCISE C
+function findSteps(scheme_id) {
+  return db('steps as st')
+  .leftJoin('schemes as sc', 'sc.scheme_id', 'st.scheme_id')
+  .select('st.step_id', 'st.step_number', 'st.instructions', 'sc.scheme_name')
+  .where('sc.scheme_id', scheme_id)
+  .orderBy('st.step_number')
   /*
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
